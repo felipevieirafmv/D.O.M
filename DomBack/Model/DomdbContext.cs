71c163947251d459126c0bc5e641508506b9b22a
Felipe de Mello Vieira <felipevieirafmv@gmail.com>
2023-11-17 13:56:42 -0300

    Connect backend with database

---
+++ DomBack/Model/DomdbContext.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DomBack.Model;
+
+public partial class DomdbContext : DbContext
+{
+    public DomdbContext()
+    {
+    }
+
+    public DomdbContext(DbContextOptions<DomdbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Imagem> Imagems { get; set; }
+
+    public virtual DbSet<Post> Posts { get; set; }
+
+    public virtual DbSet<Usuario> Usuarios { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Data Source=CT-C-001YQ\\SQLEXPRESS01;Initial Catalog=Domdb;Integrated Security=True;TrustServerCertificate=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Imagem>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Imagem__3214EC27FF0C3A47");
+
+            entity.ToTable("Imagem");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Foto).IsRequired();
+        });
+
+        modelBuilder.Entity<Post>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Post__3214EC2784B0B239");
+
+            entity.ToTable("Post");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.ImagemId).HasColumnName("ImagemID");
+            entity.Property(e => e.UsuarioId).HasColumnName("UsuarioID");
+
+            entity.HasOne(d => d.Imagem).WithMany(p => p.Posts)
+                .HasForeignKey(d => d.ImagemId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Post__ImagemID__3D5E1FD2");
+
+            entity.HasOne(d => d.Usuario).WithMany(p => p.Posts)
+                .HasForeignKey(d => d.UsuarioId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Post__UsuarioID__3C69FB99");
+        });
+
+        modelBuilder.Entity<Usuario>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Usuario__3214EC27EBE2A41C");
+
+            entity.ToTable("Usuario");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.ImagemId).HasColumnName("ImagemID");
+            entity.Property(e => e.Nome)
+                .IsRequired()
+                .HasMaxLength(80)
+                .IsUnicode(false);
+            entity.Property(e => e.Salt)
+                .IsRequired()
+                .HasMaxLength(200)
+                .IsUnicode(false);
+            entity.Property(e => e.Senha)
+                .IsRequired()
+                .IsUnicode(false);
+
+            entity.HasOne(d => d.Imagem).WithMany(p => p.Usuarios)
+                .HasForeignKey(d => d.ImagemId)
+                .HasConstraintName("FK__Usuario__ImagemI__398D8EEE");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
